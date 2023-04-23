10e1900d6fc97797a4d0a11d470f5d33fbd6ad1c
Corentin Faure <c.faure1996@gmail.com>
2023-04-23 12:08:18 +0200

    fin Paycheck

--- resources/[soz]/soz-core/src/server/bank/bank.paycheck.provider.ts
+++ resources/[soz]/soz-core/src/server/bank/bank.paycheck.provider.ts
@@ -5,13 +5,13 @@ import { JobService } from '../job.service';
 import { Tick, TickInterval } from '@public/core/decorators/tick';
 import { QBCore } from '../qbcore';
 import { Notifier } from '../notifier';
-import { BankService } from './bank.service';
+import { BankAccountService } from './bank.account.service';
 
 @Provider()
 export class BankPaycheckProvider {
 
-    @Inject(BankService)
-    private bankService: BankService;
+    @Inject(BankAccountService)
+    private bankAccountService: BankAccountService;
 
     @Inject(JobService)
     private jobService: JobService;
@@ -35,51 +35,25 @@ export class BankPaycheckProvider {
                 if (!player.PlayerData.job.onduty) {
                     payment = Math.ceil(payment * 30 / 100);
                 }
-                this.bankAccountService.transfertMoney(player.PlayerData.job.id, player.PlayerData.charinfo.account, payment, (success, reason) => {
+                this.bankAccountService.transferMoney(player.PlayerData.job.id, player.PlayerData.charinfo.account, payment, (success, reason) => {
                     if (success) {
                         this.notifier.advancedNotify(playerSource, 'Maze Banque', 'Mouvement bancaire', 'Un versement vient d\'être réalisé sur votre compte', 'CHAR_BANK_MAZE', 'success');
+                        TriggerEvent("monitor:server:event", "paycheck",
+                            {
+                                player_source: player.PlayerData.source
+                            },
+
+                            {
+                                amount: payment,
+                            });
+                    }
+                    else {
+                        console.log(reason);
                     }
                 });
 
             }
 
         }
-        /*
-
-local Players = QBCore.Functions.GetQBPlayers()
-    local SozJobCore = exports["soz-jobs"]:GetCoreObject()
-
-    for _, Player in pairs(Players) do
-        local grade = SozJobCore.Jobs[Player.PlayerData.job.id].grades[tostring(Player.PlayerData.job.grade)] or {}
-        local payment = grade.salary or 0
-
-        if Player.PlayerData.metadata["injail"] == 0 and Player.PlayerData.job and payment > 0 then
-            if Player.PlayerData.job.id == SozJobCore.JobType.Unemployed then
-                Account.AddMoney(Player.PlayerData.charinfo.account, payment)
-                NotifyPaycheck(Player.PlayerData.source)
-
-                TriggerEvent("monitor:server:event", "paycheck", {player_source = Player.PlayerData.source}, {
-                    amount = tonumber(payment),
-                })
-            else
-                if not Player.PlayerData.job.onduty then
-                    payment = math.ceil(payment * 30 / 100)
-                end
-
-                Account.TransfertMoney(Player.PlayerData.job.id, Player.PlayerData.charinfo.account, payment, function(success, reason)
-                    if success then
-                        NotifyPaycheck(Player.PlayerData.source)
-
-                        TriggerEvent("monitor:server:event", "paycheck", {player_source = Player.PlayerData.source}, {
-                            amount = tonumber(payment),
-                        })
-                    else
-                        print(reason)
-                    end
-                end)
-            end
-        end
-    end        
-        */
     }
 }
