8a11f4ebab5ac39ff8d5f2d8946688b8a3883993
Corentin FAURE <corentin.faure@s2hgroup.com>
2023-04-25 14:12:33 +0200

    WIP on INVOICES

---
+++ resources/[soz]/soz-core/src/server/bank/bank.invoice.service.ts
@@ -0,0 +1,10 @@
+import { Invoice } from '@public/shared/bank';
+import { Injectable } from '../../core/decorators/injectable';
+
+
+@Injectable()
+export class BankInvoiceService {
+    private Invoices: Invoice[] = [];
+
+    
+}
