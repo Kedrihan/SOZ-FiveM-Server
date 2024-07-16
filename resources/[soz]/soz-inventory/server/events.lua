2b7cfd9e31725dd869f2f52428cf0ff2eb8ab298
Kedrihan <c.faure1996@gmail.com>
2024-07-16 17:44:05 +0200

    ajout notification

--- resources/[soz]/soz-inventory/server/events.lua
+++ resources/[soz]/soz-inventory/server/events.lua
@@ -72,6 +72,7 @@ QBCore.Functions.CreateCallback("inventory:server:TransfertItem",
                     coord1.z,
                 });
             end
+            TriggerClientEvent("soz-core:client:notification:draw", sourceInv.id, string.format("On vous a pris ~r~%s %s", amount, QBCore.Shared.Items[item].label))
         end
 
         local sourceInventory = sourceInv
